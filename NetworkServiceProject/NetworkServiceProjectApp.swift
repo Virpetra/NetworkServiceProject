42188c17606f72e2decb815cbf20b9f1dbbcc65b
Virpetra <msaiddede@gmail.com>
2024-02-08 20:57:57 +0300

    Initial Commit

---
+++ NetworkServiceProject/NetworkServiceProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkServiceProjectApp.swift
+//  NetworkServiceProject
+//
+//  Created by Mehmet Said Dede on 8.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct NetworkServiceProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
