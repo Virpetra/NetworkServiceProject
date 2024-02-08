7d7c02aac9c31dd8ee3cb3d1831aafdef4d8a4d9
Virpetra <msaiddede@gmail.com>
2024-02-08 22:39:10 +0300

    .

--- NetworkServiceProject/HomeView.swift
+++ NetworkServiceProject/HomeView.swift
@@ -8,9 +8,34 @@
 import SwiftUI
 
 struct HomeView: View {
+    
+    @StateObject var networkManager = NetworkManager()
+    @State private var coins: [CoinModel] = []
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        
+        List(coins, id: \.id) { coin in
+            Text(coin.name)
+        }
+        .onAppear {
+            fetchData()
+        }
     }
+    func fetchData() {
+            networkManager.fetchData { result in
+                switch result {
+                case .success(let data):
+                    if let decodedData = try? JSONDecoder().decode([CoinModel].self, from: data) {
+                        DispatchQueue.main.async {
+                            self.coins = decodedData
+                        }
+                    }
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+            }
+        }
 }
 
 #Preview {
--- NetworkServiceProject/NetworkManager.swift
+++ NetworkServiceProject/NetworkManager.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+class NetworkManager : ObservableObject {
+    
+    func fetchData(completion: @escaping (Result<Data, Error>) -> Void ) {
+        guard let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&locale=en") else {
+            completion(.failure(NetworkError.invalidURL))
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data, error == nil else {
+                completion(.failure(NetworkError.invalidData))
+                return
+            }
+            completion(.success(data))
+        }.resume()
+    }
+    
+}
+
+enum NetworkError: Error {
+    case invalidURL
+    case invalidData
+}
+
+
